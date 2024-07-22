2df022525fe5373951ee1b118fb31efb0e27df9d
skiss.cc <skiss.cc@gmail.com>
2024-07-22 08:52:49 +0800

    aaaaaaa

--- const.sh
+++ const.sh
@@ -6,4 +6,4 @@ OLS_CONF_DIR=$OLS_ROOT/conf
 # 监听器下的站点
 VH_DOMAIN_FILE=$OLS_CONF_DIR/vhosts.conf
 # 仓库文件地址
-DOWNLOAD_URL=https://raw.githubusercontent.com/mina998/lswp2/main/httpd
+DOWNLOAD_URL=https://raw.githubusercontent.com/mina998/lsphp82/main/httpd
